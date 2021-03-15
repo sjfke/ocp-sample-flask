006c71f3c10f5dceb10c9bba6d8915a7db6dcdf6
sjfke <gcollis@ymail.com>
2021-03-15 17:53:17 +0100

    whoops missed these changes

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,17 @@
 from flask import Flask
-application = Flask(__name__)
+from flask import render_template
+
+application = Flask(__name__, instance_relative_config=True)
 
 
-@application.route("/")
-def hello():
-    return "Hi Blue Sphere!"
+@application.route('/')
+def index():
+    return render_template("index.html")
+
+
+@application.route('/about')
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
